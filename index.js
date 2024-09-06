e1fd1118cdf78e1d6e94d3c29d0c15a5b6c45f96
Caio Souza <caio.7s@outlook.com>
2024-09-06 17:19:22 -0300

    fix: 1ml de masteron a cada 3 dias

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ import { getRandomPost, insertPost, markPostAsPosted, initializeDatabase } from 
 import { CronJob } from "cron";
 
 const config = {
-    jpeg: { quality: 60 },
-    webp: { quality: 60 },
-    png: { compressionLevel: 2 },
-    jpg: { quality: 60 },
+	jpeg: { quality: 60 },
+	webp: { quality: 60 },
+	png: { compressionLevel: 2 },
+	jpg: { quality: 60 },
 };
 
 async function getRedditPost() {
-    try {
-        const response = await axios.get('https://www.reddit.com/r/terraria/top.json?t=hour', {
-            headers: {
+	try {
+		const response = await axios.get('https://www.reddit.com/r/terraria/top.json?t=hour', {
+			headers: {
 				accept:
 					'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 				'accept-language': 'en-US,en;q=0.9',
@@ -37,145 +37,145 @@ async function getRedditPost() {
 				'user-agent':
 					'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
 			},
-        });
+		});
 
-        const posts = response.data.data.children
-            .map((post) => ({
-                title: post.data?.title,
-                link: `redd.it/${post.data?.id}`,
-                id: post.data?.id,
-                preview: post.data?.url,
-            }))
-            .filter((post) => !post.preview.includes('gallery'))
-            .filter((post) => Object.keys(config).includes(post.preview.split('.').pop()));
+		const posts = response.data.data.children
+			.map((post) => ({
+				title: post.data?.title,
+				link: `redd.it/${post.data?.id}`,
+				id: post.data?.id,
+				preview: post.data?.url,
+			}))
+			.filter((post) => !post.preview.includes('gallery'))
+			.filter((post) => Object.keys(config).includes(post.preview.split('.').pop()));
 
-        return posts.length ? posts : null;
-    } catch (error) {
-        console.error('Error fetching reddit posts', error);
-        throw error;
-    }
+		return posts.length ? posts : null;
+	} catch (error) {
+		console.error('Error fetching reddit posts', error);
+		throw error;
+	}
 }
 
 async function downloadImage(imageUrl) {
-    try {
-        const response = await axios.get(imageUrl, {
-            responseType: 'arraybuffer',
-        });
+	try {
+		const response = await axios.get(imageUrl, {
+			responseType: 'arraybuffer',
+		});
 
-        const imageName = uuidv4();
-        const imageExtension = imageUrl.split('.').pop();
-        const buffer = Buffer.from(response.data);
+		const imageName = uuidv4();
+		const imageExtension = imageUrl.split('.').pop();
+		const buffer = Buffer.from(response.data);
 
-        const imgPath = path.join(__dirname, 'img', `${imageName}.${imageExtension}`);
-        fs.writeFileSync(imgPath, buffer);
+		const imgPath = path.join(__dirname, 'img', `${imageName}.${imageExtension}`);
+		fs.writeFileSync(imgPath, buffer);
 
-        return imgPath;
-    } catch (error) {
-        console.error('Error downloading image', error);
-        throw error;
-    }
+		return imgPath;
+	} catch (error) {
+		console.error('Error downloading image', error);
+		throw error;
+	}
 }
 
 function removeImages(imagePaths) {
-    try {
-        imagePaths.forEach((image) => {
-            fs.unlinkSync(image);
-        });
-        console.log('Removed images', imagePaths);
-    } catch (error) {
-        console.error('Error removing images', error);
-        throw error;
-    }
+	try {
+		imagePaths.forEach((image) => {
+			fs.unlinkSync(image);
+		});
+		console.log('Removed images', imagePaths);
+	} catch (error) {
+		console.error('Error removing images', error);
+		throw error;
+	}
 }
 
 async function compressImage(imagePath) {
-    try {
-        const image = sharp(imagePath);
-        const { format, width, height } = await image.metadata();
+	try {
+		const image = sharp(imagePath);
+		const { format, width, height } = await image.metadata();
 
-        const compressedImageName = uuidv4();
-        const compressedImagePath = path.join(__dirname, 'img', `${compressedImageName}.${format}`);
+		const compressedImageName = uuidv4();
+		const compressedImagePath = path.join(__dirname, 'img', `${compressedImageName}.${format}`);
 
-        if (width > height) {
-            await image[format](config[format]).resize({ width: 600 }).toFile(compressedImagePath);
-        } else {
-            await image[format](config[format]).resize({ height: 600 }).toFile(compressedImagePath);
-        }
+		if (width > height) {
+			await image[format](config[format]).resize({ width: 600 }).toFile(compressedImagePath);
+		} else {
+			await image[format](config[format]).resize({ height: 600 }).toFile(compressedImagePath);
+		}
 
-        // Destruir imagem original para liberar memória
-        image.destroy();
+		// Destruir imagem original para liberar memória
+		image.destroy();
 
-        return compressedImagePath;
-    } catch (error) {
-        console.error('Error compressing image', error);
-        throw error;
-    }
+		return compressedImagePath;
+	} catch (error) {
+		console.error('Error compressing image', error);
+		throw error;
+	}
 }
 
 function getBlob(imagePath) {
-    try {
-        const imageBuffer = fs.readFileSync(imagePath);
-        const imageFormat = imagePath.split('.').pop();
-        return new Blob([imageBuffer], { type: `image/${imageFormat}` });
-    } catch (error) {
-        console.error('Error reading image file:', error);
-        throw error;
-    }
+	try {
+		const imageBuffer = fs.readFileSync(imagePath);
+		const imageFormat = imagePath.split('.').pop();
+		return new Blob([imageBuffer], { type: `image/${imageFormat}` });
+	} catch (error) {
+		console.error('Error reading image file:', error);
+		throw error;
+	}
 }
 
 async function postOnBluesky() {
-    try {
-        const bot = new Bot();
-        await bot.login({
-            identifier: process.env.IDENTIFIER,
-            password: process.env.PASSWORD,
-        });
+	try {
+		const bot = new Bot();
+		await bot.login({
+			identifier: process.env.IDENTIFIER,
+			password: process.env.PASSWORD,
+		});
 
-        const post = getRandomPost();
-        const imagePath = await downloadImage(post.preview);
-        const compressedImagePath = await compressImage(imagePath);
+		const post = getRandomPost();
+		const imagePath = await downloadImage(post.preview);
+		const compressedImagePath = await compressImage(imagePath);
 
-        console.log('Post:', post);
+		console.log('Post:', post);
 
-        await bot.post({
-            text: `${post.title} ${post.link}`,
-            images: [{ data: getBlob(compressedImagePath) }],
-        });
+		await bot.post({
+			text: `${post.title} ${post.link}`,
+			images: [{ data: getBlob(compressedImagePath) }],
+		});
 
-        removeImages([imagePath, compressedImagePath]);
-        markPostAsPosted(post.id);
-    } catch (error) {
-        console.error('Error posting on Bluesky', error);
-        throw error;
-    }
+		removeImages([imagePath, compressedImagePath]);
+		markPostAsPosted(post.id);
+	} catch (error) {
+		console.error('Error posting on Bluesky', error);
+		throw error;
+	}
 }
 
 async function populateDb() {
-    try {
-        const posts = await getRedditPost();
+	try {
+		const posts = await getRedditPost();
 
-        if (!posts) {
-            console.log('No posts to insert');
-            return;
-        }
+		if (!posts) {
+			console.log('No posts to insert');
+			return;
+		}
 
-        posts.forEach((post) => {
-            insertPost(post);
-        });
-    } catch (error) {
-        console.error('Error populating database', error);
-    }
+		posts.forEach((post) => {
+			insertPost(post);
+		});
+	} catch (error) {
+		console.error('Error populating database', error);
+	}
 }
 
 initializeDatabase(); // Iniciar a instância do banco de dados
 
 const job = new CronJob('0 * * * *', async () => {
-    try {
-        await populateDb();
-        await postOnBluesky();
-    } catch (error) {
-        console.error('Error during cron job execution', error);
-    }
+	try {
+		await populateDb();
+		await postOnBluesky();
+	} catch (error) {
+		console.error('Error during cron job execution', error);
+	}
 }, null, true, 'America/Sao_Paulo');
 
 job.start();
