a6da16dd0cf9f0c2a934a61d7b508faa11d3711a
Caio Souza <caio.7s@outlook.com>
2024-09-06 16:54:33 -0300

    fix: uohhh i'm so excited xP

--- functions.js
+++ functions.js
@@ -1,20 +1,23 @@
 import {Database} from "bun:sqlite";
 
+let db;
+
+export function initializeDatabase() {
+    db = new Database("db.sqlite");
+}
+
 export function getRandomPost() {
-    using db = new Database("db.sqlite");
-    using query = db.query("SELECT * FROM posts WHERE posted = 0 ORDER BY RANDOM() LIMIT 1");
+    const query = db.query("SELECT * FROM posts WHERE posted = 0 ORDER BY RANDOM() LIMIT 1");
     return query.get();
 }
 
 export function getPosts() {
-    using db = new Database("db.sqlite");
-    using query = db.query("SELECT * FROM posts");
+    const query = db.query("SELECT * FROM posts");
     return query.all();
 }
 
 export function markPostAsPosted(postId) {
-    using db = new Database("db.sqlite");
-    using query = db.query(`
+    const query = db.query(`
         UPDATE posts
         SET posted = 1
         WHERE id = ?
@@ -23,11 +26,9 @@ export function markPostAsPosted(postId) {
 }
 
 export function insertPost(post) {
-    using db = new Database("db.sqlite");
-    using query = db.query(`
+    const query = db.query(`
         INSERT OR IGNORE INTO posts (id, posted, link, preview, title)
         VALUES (?, ?, ?, ?, ?)
     `);
-
     query.run(post.id, false, post.link, post.preview, post.title);
 }
